dcf782cdcd423763ee71d6d924d157aa5c82987c
loveloliii <woshigegefengleng@live.com>
2021-04-27 10:53:06 +0800

    fish

---
+++ src/main/java/monster/loli/justice_eternal/util/FishUtils.java
@@ -0,0 +1,15 @@
+package monster.loli.justice_eternal.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FishUtils {
+
+    private final static Logger logger = LoggerFactory.getLogger(FishUtils.class);
+
+    public static void main(String[] args) {
+        logger.info("this is  a fish");
+
+    }
+
+}
