87771774804f75e69d21813166083d147e19abbb
loveloliii <woshigegefengleng@live.com>
2021-04-30 15:08:54 +0800

    update auto_deploy shell

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -1,3 +1,4 @@
+git pull
 P_ID=$(ps -ef | grep -w justice_eternal-0.0.1-SNAPSHOT.jar | grep -v "grep" | awk '{print $2}')
         if [ "$P_ID" == "" ]; then
                 echo "===justice_eternal-0.0.1-SNAPSHOT.jar process not exists or stop success"
